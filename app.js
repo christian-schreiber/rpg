5ff82eb9f848f62eeddcbfbb86cdecc2c6c75ad4
Christian Schreiber <christian@my365way.com>
2023-08-27 17:42:29 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const path = require('path');
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World! Using GitHub Actions</h1>')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+  res.status(404);
+  res.send('<h1>404: Page not found</h1>');
+  next();
+
 })
 
 app.listen(port, () => {
