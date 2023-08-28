93b94f555a5a1efe50be9f91b7d50eb0bd8a8d3b
Christian Schreiber <christian@my365way.com>
2023-08-28 07:54:58 +0200

    secure API-8

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ const app = express();
 const port = 5000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.use((req, res, next) => {
   res.status(404);
   res.send('<h1>404: Page not found</h1>');
   next();
-
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
